5f37c5f73cb1b26b3117cf3a20331c90db052199
Franklin Gil <imfranq.ve@gmail.com>
2022-07-10 20:37:01 +0000

    :sparkles: Inertia react setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    render(
+      <ChakraProvider>
+        <App {...props} />
+      </ChakraProvider> , el
+    )
+  },
+})
