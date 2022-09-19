94398e888abff5bf5769bb0aa03efcb1fbecf4f3
Franklin Gil <imfranq.ve@gmail.com>
2022-09-19 00:38:56 -0400

    bug fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\HasRenderizableView;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    use HasRenderizableView;
+    
     public function __construct()
     {
         $this->middleware('auth');
